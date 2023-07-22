238540f0ae4c6fa4edb622f44d78dd85347b7751
Aslam, A. (Abuzar) <abuzar.aslam@ing.com>
2023-07-22 21:52:42 +0200

    Move the api ket to NDK ..

--- app/src/main/java/com/example/albertheijnsampleapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/albertheijnsampleapp/presentation/MainActivity.kt
@@ -25,7 +25,6 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
